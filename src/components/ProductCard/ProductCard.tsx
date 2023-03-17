0b26e46570181e3bf2d6126a3cdaaa1454c2138a
Adilet <baryktabasovadi@gmail.com>
2023-03-17 22:01:39 +0600

    added form for ProductCard

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import classes from "./ProductCard.module.scss";
+import clothe1 from "./assets/Rectangle 4206.png";
+import LikeOn from "./assets/LikeOn.svg";
+import LikeOff from "./assets/LikeOff.svg";
+
+type Props = {};
+
+const ProductCard = (props: Props) => {
+  const [Like, setLike] = useState(false);
+
+  return (
+    <div className={classes.cardBlock}>
+      <div className={classes.Like}>
+        {Like ? (
+          <img
+            src={LikeOn}
+            alt=""
+            onClick={() => {
+              setLike(!Like);
+            }}
+          />
+        ) : (
+          <img
+            src={LikeOff}
+            alt=""
+            onClick={() => {
+              setLike(!Like);
+            }}
+          />
+        )}
+      </div>
+      <img src={clothe1} alt="error" />
+      <div>
+        <h3>Свитшот</h3>
+        <h3>20шт</h3>
+      </div>
+      <div>
+        <h3>Цена</h3>
+        <h3>500сом</h3>
+      </div>
+      <button>Подробнее</button>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/CardList/CardList.tsx
+++ src/pages/CardList/CardList.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import classes from "./CardList.module.scss";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
 type Props = {};
 
 const CardList = (props: Props) => {
-  return <div>CardList</div>;
+  return (
+    <div className={classes.container}>
+      <div>
+        Последние товары
+        <div>
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CardList;
