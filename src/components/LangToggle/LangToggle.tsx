cbdafbe47029cd046a0dd8629e9d80584c2be4e2
Adilet <baryktabasovadi@gmail.com>
2023-03-28 21:27:07 +0600

    added toogle

--- src/components/LangToggle/LangToggle.tsx
+++ src/components/LangToggle/LangToggle.tsx
@@ -6,18 +6,20 @@ import styled from "@emotion/styled";
 
 const LangToggle = () => {
   return (
-    <div className={dropdownStyled.Dropdown}>
-      <div className="dropdown-element" data-lang-code="ru">
-        RU
-      </div>
-
-      <div className="dropdown-element" data-lang-code="en">
-        EN
-      </div>
-
-      <div className="dropdown-element" data-lang-code="kg">
-        KG
-      </div>
+    <div>
+      <TextField
+        className="inputRounded "
+        id="outlined-select-currency"
+        select
+        defaultValue="RU"
+        SelectProps={{ IconComponent: () => null }}>
+        <MenuItem key="ru" value="RU">
+          RU
+        </MenuItem>
+        <MenuItem key="en" value="EN">
+          EN
+        </MenuItem>
+      </TextField>
     </div>
   );
 };
